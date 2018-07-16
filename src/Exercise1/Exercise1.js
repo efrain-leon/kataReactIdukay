8b133c690e5a82b2e6bd8dde9c9400702342983a
Efrain leon <efrain.leonh.trabajo@gmail.com>
2018-07-16 10:37:17 -0500

    added code for resolve labyrinth

--- src/Exercise1/Exercise1.js
+++ src/Exercise1/Exercise1.js
@@ -6,6 +6,105 @@ export class Exercise1 extends Component {
     this.state = {};
   }
   
+  labyrinth() {
+    return [['Inicio', 0, 1], [1, 0, 1], [1, 0, 'Salida']];
+  }
+  
+  labyrinth2() {
+    return [['Inicio', 0, 0, 1, 'Salida'], [1, 0, 1, 1, 0], [1, 0, 0, 1, 0], [0, 0, 1, 0, 0], [1, 0, 0, 0, 0]];
+  }
+  
+  getCoordinatesStartAndEnd(labyrinth) {
+    let start = {};
+    let end = {};
+    
+    for (let i = 0; i < labyrinth.length; i += 1) {
+      for (let j = 0; j < labyrinth.length; j += 1) {
+        if (labyrinth[i][j] === 'Inicio') {
+          start = {x: i, y: j};
+        }
+        
+        if (labyrinth[i][j] === 'Salida') {
+          end = {x: i, y: j};
+        }
+      }
+    }
+    
+    return {start: start, end: end};
+  }
+  
+  getHomologateLabyrinth(labyrinth) {
+    for (let i = 0; i < labyrinth.length; i += 1) {
+      for (let j = 0; j < labyrinth.length; j += 1) {
+        if (labyrinth[i][j] === 'Inicio') {
+          labyrinth[i][j] = 0;
+        }
+        
+        if (labyrinth[i][j] === 'Salida') {
+          labyrinth[i][j] = 0;
+        }
+      }
+    }
+    
+    return labyrinth;
+  }
+  
+  findPath(labyrinth, coordinates) {
+    let posx = coordinates.start.x;
+    let posy = coordinates.start.y;
+    let stack = [];
+    let pathFound = false;
+    
+    while (!pathFound) {
+      stack.push({x: posx, y: posy});
+      
+      if (posx === coordinates.end.x && posy === coordinates.end.y) {
+        break;
+      } else {
+        labyrinth[posx][posy] = 1;
+        
+        if (labyrinth[posx + 1] && labyrinth[posx + 1][posy] === 0) {
+          posx += 1;
+          continue;
+        }
+          
+        if (labyrinth[posx][posy + 1] === 0) {
+          posy += 1;
+          continue;
+        }
+          
+        if (labyrinth[posx - 1] && labyrinth[posx - 1][posy] === 0) {
+          posx -= 1;
+          continue;
+        }
+        
+        if (labyrinth[posx][posy - 1] === 0) {
+          posy -= 1;
+          continue;
+        }
+        
+        stack.pop();
+          
+        if (stack.length > 0) {
+          posx = stack.pop()[0];
+          posy = stack.pop()[1];
+        } else {
+          break;
+        }
+      }
+    }
+    
+    return stack;
+  }
+  
+  printPath(labyrinth, stack) {
+    stack.map(element => {
+      labyrinth[element.x][element.y] = 'x';
+    });
+    
+    return labyrinth;
+  }
+  
   render() {
     return (
       <div className="container">
--- src/Exercise1/specs/Exercise1.spec.js
+++ src/Exercise1/specs/Exercise1.spec.js
@@ -16,4 +16,119 @@ describe('Exercise1', () => {
       expect(instance.state).toEqual({});
     });
   });
+  
+  describe('when matrix is 3x3', () => {
+    let labyrinth;
+    
+    beforeEach(() => {
+      labyrinth = instance.labyrinth();
+    });
+    
+    it('should create the labyrinth', () => {
+      expect(labyrinth[0]).toEqual(['Inicio', 0, 1]);
+      expect(labyrinth[1]).toEqual([1, 0, 1]);
+      expect(labyrinth[2]).toEqual([1, 0, 'Salida']);
+    });
+    
+    it('should return coordinates of start and end', () => {
+      const result = instance.getCoordinatesStartAndEnd(labyrinth);
+      
+      expect(result.start).toEqual({x: 0, y: 0});
+      expect(result.end).toEqual({x: 2, y: 2});
+    });
+    
+    it('should return homologate labyrinth with 0 and 1', () => {
+      const result = instance.getHomologateLabyrinth(labyrinth);
+      
+      expect(result[0]).toEqual([0, 0, 1]);
+      expect(result[1]).toEqual([1, 0, 1]);
+      expect(result[2]).toEqual([1, 0, 0]);
+    });
+    
+    it('should found path', () => {
+      const coordinates = instance.getCoordinatesStartAndEnd(labyrinth);
+      let result = instance.getHomologateLabyrinth(labyrinth);
+      result = instance.findPath(result, coordinates);
+      
+      expect(result).toEqual([{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}, {x: 2, y: 2}]);
+    });
+    
+    it('should print path', () => {
+      const coordinates = instance.getCoordinatesStartAndEnd(labyrinth);
+      labyrinth = instance.getHomologateLabyrinth(labyrinth);
+      const path = instance.findPath(labyrinth, coordinates);
+      const result = instance.printPath(labyrinth, path);
+      
+      expect(result[0]).toEqual(['x', 'x', 1]);
+      expect(result[1]).toEqual([1, 'x', 1]);
+      expect(result[2]).toEqual([1, 'x', 'x']);
+    });
+  });
+  
+  describe('when matrix is 5x5', () => {
+    let labyrinth;
+    
+    beforeEach(() => {
+      labyrinth = instance.labyrinth2();
+    });
+    
+    it('should create the labyrinth', () => {
+      expect(labyrinth[0]).toEqual(['Inicio', 0, 0, 1, 'Salida']);
+      expect(labyrinth[1]).toEqual([1, 0, 1, 1, 0]);
+      expect(labyrinth[2]).toEqual([1, 0, 0, 1, 0]);
+      expect(labyrinth[3]).toEqual([0, 0, 1, 0, 0]);
+      expect(labyrinth[4]).toEqual([1, 0, 0, 0, 0]);
+    });
+    
+    it('should return coordinates of start and end', () => {
+      const result = instance.getCoordinatesStartAndEnd(labyrinth);
+      
+      expect(result.start).toEqual({x: 0, y: 0});
+      expect(result.end).toEqual({x: 0, y: 4});
+    });
+    
+    it('should return homologate labyrinth with 0 and 1', () => {
+      labyrinth = instance.getHomologateLabyrinth(labyrinth);
+      
+      expect(labyrinth[0]).toEqual([0, 0, 0, 1, 0]);
+      expect(labyrinth[1]).toEqual([1, 0, 1, 1, 0]);
+      expect(labyrinth[2]).toEqual([1, 0, 0, 1, 0]);
+      expect(labyrinth[3]).toEqual([0, 0, 1, 0, 0]);
+      expect(labyrinth[4]).toEqual([1, 0, 0, 0, 0]);
+    });
+    
+    it('should found path', () => {
+      const coordinates = instance.getCoordinatesStartAndEnd(labyrinth);
+      let result = instance.getHomologateLabyrinth(labyrinth);
+      result = instance.findPath(result, coordinates);
+      
+      expect(result).toEqual([
+        {x: 0, y: 0},
+        {x: 0, y: 1},
+        {x: 1, y: 1},
+        {x: 2, y: 1},
+        {x: 3, y: 1},
+        {x: 4, y: 1},
+        {x: 4, y: 2},
+        {x: 4, y: 3},
+        {x: 4, y: 4},
+        {x: 3, y: 4},
+        {x: 2, y: 4},
+        {x: 1, y: 4},
+        {x: 0, y: 4}]);
+    });
+    
+    it('should print path', () => {
+      const coordinates = instance.getCoordinatesStartAndEnd(labyrinth);
+      labyrinth = instance.getHomologateLabyrinth(labyrinth);
+      const path = instance.findPath(labyrinth, coordinates);
+      const result = instance.printPath(labyrinth, path);
+      
+      expect(result[0]).toEqual(['x', 'x', 0, 1, 'x']);
+      expect(result[1]).toEqual([1, 'x', 1, 1, 'x']);
+      expect(result[2]).toEqual([1, 'x', 0, 1, 'x']);
+      expect(result[3]).toEqual([0, 'x', 1, 0, 'x']);
+      expect(result[4]).toEqual([1, 'x', 'x', 'x', 'x']);
+    });
+  });
 });
